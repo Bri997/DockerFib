8afadc2ecdaa35071b4bc9273e2fbdc66dc9a55f
Brian <brian.musial@gmail.com>
2019-07-09 21:22:23 -0700

    fixed package.json

--- server/index.js
+++ server/index.js
@@ -38,7 +38,7 @@ app.get("/", (req, res)=> {
 });
 
 app.get("/values/all", async(req, res)=>{
-    const values = await pgClientClient.query("SELECT * from values")
+    const values = await pgClient.query("SELECT * from values")
 
     res.send(values.rows)
 })
